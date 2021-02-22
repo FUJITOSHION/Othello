b382dbbe33343547ae57cca4b54333287533510f
fujitoshion <shion@shion.local>
2021-02-23 03:31:39 +0900

    [fix] 使わないファイル削除やリファクタリングをした

--- src/components/greet.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react"
-
-interface GreetProps {
-  name: string
-}
-
-export const Greet: React.FC<GreetProps> = ({ name }: GreetProps) => {
-  return <div>Hello! {name}</div>
-}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,38 +1,45 @@
+import Head from "next/head"
 import { useState } from "react"
 
 import styles from "@styles/modules/Home.module.scss"
 
 export default function Home(): JSX.Element {
   const [kagawa, setKagawa] = useState<boolean>(false)
-  const [useCpu, setUseCpu] = useState<boolean>(false)
+  const [isAgreeCpu, setIsAgreeCpu] = useState<boolean>(false)
 
-  const handlekagawa = () => {
-    const nextKagawa = !kagawa
-    setKagawa(nextKagawa)
+  const handleKagawa = () => {
+    setKagawa(!kagawa)
   }
 
-  const handleUseCpu = () => {
-    const nextUseCpu = !useCpu
-    setUseCpu(nextUseCpu)
+  const handleIsAgreeCpu = () => {
+    setIsAgreeCpu(!isAgreeCpu)
   }
   return (
     <div>
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <div>オセロゲーム</div>
       <ul className={styles.checkList}>
         <li>
           <label>
-            <input type="checkbox" checked={kagawa} onChange={handlekagawa} />
+            <input type="checkbox" checked={kagawa} onChange={handleKagawa} />
             香川県民ではありません。*香川県の条例に基づく
           </label>
         </li>
         <li>
           <label>
-            <input type="checkbox" checked={useCpu} onChange={handleUseCpu} />
+            <input
+              type="checkbox"
+              checked={isAgreeCpu}
+              onChange={handleIsAgreeCpu}
+            />
             cpuを利用することに同意します。
           </label>
         </li>
       </ul>
-      <button disabled={!(kagawa && useCpu)}>ゲームを開始する</button>
+      <button disabled={!(kagawa && isAgreeCpu)}>ゲームを開始する</button>
     </div>
   )
 }
