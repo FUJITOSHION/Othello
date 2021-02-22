15d7415b0681b3ec1ff9eee80fbfbe4e66bf9de4
fujitoshion <shion@shion.local>
2021-02-23 02:47:55 +0900

    [add] #2 スタート画面の機能を実装しました

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,69 +1,40 @@
 import Head from "next/head"
+import { useState } from "react"
 
 import { Greet } from "@components/greet"
 import styles from "@styles/modules/Home.module.scss"
 
 export default function Home(): JSX.Element {
+  const [kagawa, setKagawa] = useState<boolean>(false)
+  const [useCpu, setUseCpu] = useState<boolean>(false)
+
+  const handlekagawa = () => {
+    const nextKagawa = !kagawa
+    setKagawa(nextKagawa)
+  }
+
+  const handleUseCpu = () => {
+    const nextUseCpu = !useCpu
+    setUseCpu(nextUseCpu)
+  }
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <Greet name={`Taro!`} />
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{` `}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{` `}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
+    <div>
+      <div>オセロゲーム</div>
+      <ul className={styles.checkList}>
+        <li>
+          <label>
+            <input type="checkbox" checked={kagawa} onChange={handlekagawa} />
+            香川県民ではありません。*香川県の条例に基づく
+          </label>
+        </li>
+        <li>
+          <label>
+            <input type="checkbox" checked={useCpu} onChange={handleUseCpu} />
+            cpuを利用することに同意します。
+          </label>
+        </li>
+      </ul>
+      <button disabled={!(kagawa && useCpu)}>ゲームを開始する</button>
     </div>
   )
 }
