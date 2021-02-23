4b38d2e517c641e82c9b0844da6dba043bd006cc
kimuson <dev.kimuson@gmail.com>
2021-02-23 18:00:33 +0900

    [add] 試合終了判定ロジックを追加した

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,6 +1,13 @@
-export type Player = "ai" | "opponent"
+const aiStr = "ai"
+const opponentStr = "opponent"
+
+export type Player = typeof aiStr | typeof opponentStr
 export type CellState = Player | undefined
 export type BoardIndex = [number, number]
+export type Score = {
+  [aiStr]: number
+  [opponentStr]: number
+}
 
 export type GameState = {
   boardState: CellState[][]
