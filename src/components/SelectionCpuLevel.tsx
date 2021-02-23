66921f93a6c00cbf8d7698b10a7d041b6df3a125
fujitoshion <shion@shion.local>
2021-02-23 15:00:58 +0900

    [add] #4 スタート画面のcpuの難易度調整のpulldownの実装をしました

--- src/components/SelectionCpuLevel.tsx
+++ src/components/SelectionCpuLevel.tsx
@@ -1,9 +1,23 @@
+import { useState } from "react"
+type Level = "強い" | "普通" | "弱い"
+
 export const SelectionCpuLevel: React.FC = () => {
+  const [level, setLevel] = useState<Level>("強い")
+
+  const handleLevelChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    const value = e.target.value as Level
+    setLevel(value)
+  }
   return (
-    <select name="level">
-      <option value="強い">強い</option>
-      <option value="普通">普通</option>
-      <option value="弱い">弱い</option>
-    </select>
+    <div>
+      <span>CPUの強さを選んでください</span>
+      <select name="level" onChange={handleLevelChange} value={level}>
+        {["強い", "普通", "弱い"].map((lev) => (
+          <option value={lev} key={lev}>
+            {lev}
+          </option>
+        ))}
+      </select>
+    </div>
   )
 }
