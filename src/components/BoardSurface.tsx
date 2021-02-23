e9ab0f4c5752823ceb122e64023859b68a4fc2c1
fujitoshion <shion@shion.local>
2021-02-23 20:19:10 +0900

    #8 オセロの盤面を表示

--- src/components/BoardSurface.tsx
+++ src/components/BoardSurface.tsx
@@ -1,12 +1,49 @@
 import { Cell } from "./Cell"
+import { CellState } from "../../src/types/index"
+import { range, includes } from "ramda"
+import { validIndexes } from "../utils/game/board-index"
 
 export const BoardSurface: React.FC = () => {
+  const isAiWhite = true
+  const cells: CellState[][] = range(0, 10).map((i) =>
+    range(0, 10).map((j) => undefined)
+  )
+  if (isAiWhite) {
+    cells[4][4] = "ai"
+    cells[4][5] = "opponent"
+    cells[5][5] = "ai"
+    cells[5][4] = "opponent"
+  } else {
+    cells[4][4] = "opponent"
+    cells[4][5] = "ai"
+    cells[5][5] = "opponent"
+    cells[5][4] = "ai"
+  }
   return (
-    <div>
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+      }}
+    >
+      {cells.map((line, i) => (
+        <div style={{ display: "flex", flexDirection: "row" }} key={i}>
+          {line.map((cell, j) => (
+            <Cell
+              state={cell}
+              key={j}
+              isValid={includes(
+                [i, j],
+                validIndexes({
+                  boardState: cells,
+                  nextPlayer: "opponent",
+                })
+              )}
+            />
+          ))}
+        </div>
+      ))}
       <p>hello</p>
-      <Cell state={"ai"} />
-      <Cell state={"opponent"} />
-      <Cell state={undefined} />
     </div>
   )
 }
--- src/components/Cell.tsx
+++ src/components/Cell.tsx
@@ -2,21 +2,23 @@ import { CellState } from "../types/index"
 
 type Props = {
   state: CellState
+  isValid: boolean
 }
 
-type CellDisplayType = "白" | "黒" | undefined
+// type CellDisplayType = "白" | "黒" | undefined
 
-export const Cell: React.FC<Props> = ({ state }: Props) => {
+export const Cell: React.FC<Props> = ({ state, isValid }: Props) => {
   // ここのisAIWhiteをglobalでもつ
   const isAIWhite = true
-
+  console.log(isValid)
+  console.log(state)
   return (
-    <div>
+    <div style={{ border: "medium solid #000000", backgroundColor: "#336f33" }}>
       {typeof state === "string"
         ? (state === "ai" && isAIWhite) || (state === "opponent" && !isAIWhite)
           ? "白"
           : "黒"
-        : null}
+        : "他"}
     </div>
   )
 }
