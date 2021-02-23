27e08976e394a16bb11afcfd3996e466310418bc
fujitoshion <shion@shion.local>
2021-02-23 15:47:44 +0900

    [add] ゲームページにも難易度pulldown　wo

--- src/pages/game.tsx
+++ src/pages/game.tsx
@@ -1,5 +1,10 @@
 import styles from "@styles/modules/Home.module.scss"
+import { SelectionCpuLevel } from "../components/SelectionCpuLevel"
 
 export default function Game(): JSX.Element {
-  return <div>a</div>
+  return (
+    <div>
+      <SelectionCpuLevel />
+    </div>
+  )
 }
