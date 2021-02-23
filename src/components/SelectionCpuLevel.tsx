91488e3307d0ab3d8015d5a203551e789477447d
fujitoshion <shion@shion.local>
2021-02-23 15:56:14 +0900

    [fix] import文の修正とリファクタ

--- src/components/SelectionCpuLevel.tsx
+++ src/components/SelectionCpuLevel.tsx
@@ -1,12 +1,12 @@
 import { useState } from "react"
+
 type Level = "強い" | "普通" | "弱い"
 
 export const SelectionCpuLevel: React.FC = () => {
   const [level, setLevel] = useState<Level>("強い")
 
   const handleLevelChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
-    const value = e.target.value as Level
-    setLevel(value)
+    setLevel(e.target.value as Level)
   }
   return (
     <div>
